0dc1109b8a73df971a87e72852ba18ae702f17ee
megabombus-rex <czechumennakompie@gmail.com>
2026-01-02 13:09:36 +0100

    added base kfold and model wrapper

---
+++ src/scripts/experimental/wrappers/experiment_model.py
@@ -0,0 +1,13 @@
+class BaseModel():
+    def __init__(self, model):
+        pass
+    
+    def __call__(self, x):
+        pass
+    
+    # fitting, training etc.
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
