84ac80544383e1a286a39a7006f4677aef1585d2
megabombus-rex <czechumennakompie@gmail.com>
2026-01-10 15:56:58 +0100

    extended base model

--- src/scripts/experimental/wrappers/experiment_model.py
+++ src/scripts/experimental/wrappers/experiment_model.py
@@ -1,5 +1,8 @@
+import pytorch_lightning as L
+
 class BaseModel:
-    def __init__(self, model):
+    def __init__(self, model: L.LightningModule):
+        self.model = model
         pass
 
     def __call__(self, x):
@@ -9,5 +12,9 @@ class BaseModel:
     def fit(self, X, y):
         pass
 
-    def predict(self, X):
+    def predict(self, x):
+        pass
+    
+    # will probably do the same, but without batch
+    def predict_batch(self, x, batch_size):
         pass
